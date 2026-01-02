0d1e27329e1e68ecfbb1ec734a7b55e060de83de
Devesh_saraogi <dev.saraogi@outlook.com>
2026-01-02 16:39:53 +0530

    second Commit

--- SO_LLM.py
+++ SO_LLM.py
@@ -5,6 +5,9 @@ load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 import os
+import base64
+
+
 #schema
 class ImageMetadata(BaseModel):
     category: str = Field(description="Primary category of the product")
@@ -13,8 +16,11 @@ class ImageMetadata(BaseModel):
     material: Optional[str] = Field(description="Material of the product")
     gender: Optional[str] = Field(description="Target gender if applicable")
     style: Optional[str] = Field(description="Style like casual, formal, sporty")
+    occasion : Optional[str] = Field(description="Most Probable Occasion to Wear the Product")
     description: str = Field(description="One sentence product description")
-import base64
+
+
+
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
